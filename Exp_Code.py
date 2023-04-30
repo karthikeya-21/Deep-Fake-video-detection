eb1e07d13f09fa10d96ae2a673b37aba2e93df5d
DShivaram01 <117584247+DShivaram01@users.noreply.github.com>
2023-04-30 23:03:19 +0530

    Update Exp_Code.py

--- Exp_Code.py
+++ Exp_Code.py
@@ -1,1 +1,55 @@
+#Import required libraries
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Input, Flatten, Dense, Dropout
+from tensorflow.keras.applications import VGG19, ResNet50, EfficientNetB7
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+#Preprocessing data by extracting features and labelling them
+train_dir = 'path/to/train/data' 
+validation_dir = 'path/to/validation/data'
+test_dir = 'path/to/test/data'
+
+# Set batch size and image dimensions
+batch_size = 32
+img_height = 224
+img_width = 224
+
+# Create data generators with data augmentation and normalization
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=10,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1,
+    horizontal_flip=True
+)
+validation_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir, # <-path to training directory
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir, # <-path to validation directory
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir, # <-path to testing directory
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='binary'
+)
 
