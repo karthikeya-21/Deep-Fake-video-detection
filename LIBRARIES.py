e1f077038abcb95cadabbba0b1108133656c4a63
DShivaram01 <117584247+DShivaram01@users.noreply.github.com>
2023-04-30 22:59:25 +0530

    Delete LIBRARIES.py

--- LIBRARIES.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import Input, Flatten, Dense, Dropout
-from tensorflow.keras.applications import VGG19, ResNet50, EfficientNetB7
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
